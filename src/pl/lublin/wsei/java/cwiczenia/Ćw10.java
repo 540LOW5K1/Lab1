1f8749627089d39eb17daaaac9c741441a1ba075
540LOW5K1 <eryksadlowski@gmail.com>
2022-05-28 00:05:43 +0200

    Program do wypisania postaci dwójkowej i szesnastkowej

--- src/pl/lublin/wsei/java/cwiczenia/Ćw10.java
+++ src/pl/lublin/wsei/java/cwiczenia/Ćw10.java
@@ -1,5 +1,6 @@
 package pl.lublin.wsei.java.cwiczenia;
 
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Ćw10 {
@@ -12,12 +13,12 @@ public class Ćw10 {
         int x;
         System.out.print("Podaj liczbę: ");
         x = input.nextInt();
-        String bin = leftPad(integer.toBinaryString(x), '0', 8);
-        String hex = leftPad(integer.toHexString(x), '0', 4);
+        String bin = leftPad(integer.toBinaryString(x).toUpperCase(Locale.ROOT), '0', 8);
+        String hex = leftPad(integer.toHexString(x).toUpperCase(Locale.ROOT), '0', 4);
         System.out.printf("DEC = %d, BIN = %s, HEX =%s", x, bin, hex);
     }
 
-    private static String leftPad(String aText, char aChar, int aWidth) {
+    public static String leftPad(String aText, char aChar, int aWidth) {
         String res = aText;
         for (int i = 0; i < aWidth - aText.length(); i++)
             res = aChar + res;
