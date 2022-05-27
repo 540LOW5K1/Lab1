556f186e663d91aef7bdb2f0fdd0adcbf40f53c9
540LOW5K1 <eryksadlowski@gmail.com>
2022-05-27 16:45:01 +0200

    Tabela logarytmów

--- src/pl/lublin/wsei/java/cwiczenia/Ćw7.java
+++ src/pl/lublin/wsei/java/cwiczenia/Ćw7.java
@@ -4,9 +4,16 @@ public class Ćw7 {
 
     public static void main(String[] args) {
 
-        System.out.printf("alfa\tsin(alfa)\n");
-        for (int i = 0; i < 370; i+=10)
-            System.out.printf("%d\t%f\t\n", i, Math.sin(i/360.0*2*Math.PI));
+        System.out.printf("\narg\t\tlog2(arg)\tsum(arg)\n");
+
+        int arg = 1, sum = 0;
+        double log;
+        do {
+            log = Math.log(arg) / Math.log(2);
+            sum += arg;
+            System.out.printf("%d\t\t\t%.0f\t\t\t%d\n", arg, log, sum);
+            arg *= 2;
+        } while (arg < 8);
 
     }
 }
